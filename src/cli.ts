0ebb5b8de0f23bf5ebea63ca2718af4435178b1d
sarahmcdougall <smcdougall@mitre.org>
2022-11-21 15:15:13 -0500

    find abs path from rel path

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { resolve } from 'path';
+import fs from 'fs';
 import { BulkDataClient as Types } from 'bulk-data-client';
 import BulkDataClient from 'bulk-data-client/built/lib/BulkDataClient';
 import CLIReporter from 'bulk-data-client/built/reporters/cli';
@@ -10,12 +12,18 @@ const program = new Command();
 program
   .requiredOption('-f, --fhir-url <url>', 'Base URL of FHIR server used for data retrieval')
   .requiredOption('-g, --group <id>', 'FHIR Group ID used to query FHIR server for resources')
-  .option('-d, --destination <destination>', 'Download destination. Defaults to ./downloads', './downloads')
+  .option(
+    '-d, --destination <destination>',
+    'Download destination relative to current working directory. Defaults to ./downloads',
+    `${process.cwd()}/downloads`
+  )
   .option('-p, --parallel-downloads <number>', 'Number of downloads to run in parallel. Defaults to 1.', '1')
   .parseAsync(process.argv);
 
 // add required trailing slash to FHIR URL if not present
 program.opts().fhirUrl = program.opts().fhirUrl.replace(/\/*$/, '/');
+// get absolute path for specified destination directory
+program.opts().destination = resolve(program.opts().destination);
 
 const main = async () => {
   const requests = {
@@ -33,6 +41,11 @@ const main = async () => {
     ...program.opts(),
     requests,
   } as Types.NormalizedOptions;
+
+  if (!fs.existsSync(program.opts().destination)) {
+    fs.mkdirSync(program.opts().destination, { recursive: true });
+  }
+
   const client = new BulkDataClient(options as Types.NormalizedOptions);
   CLIReporter(client);
   const statusEndpoint = await client.kickOff();
