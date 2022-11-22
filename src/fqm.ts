92b1e3230143d9516a0746381c66b6396e404248
radamson <radamson@users.noreply.github.com>
2022-11-22 11:45:19 -0500

    initial measure calculation

--- src/fqm.ts
+++ src/fqm.ts
@@ -1,9 +1,14 @@
 import { readFile } from 'fs/promises';
+import { Calculator } from 'fqm-execution';
 import * as path from 'path';
+import { CalculationOptions } from 'fqm-execution/build/types/Calculator';
 
-export const loadCMS122 = async (): Promise<any> => {
+export const loadCMS122 = async (): Promise<fhir4.Bundle> => {
   const data = await readFile(path.join(__dirname, 'measures', 'DiabetesHemoglobinA1cHbA1cPoorControl9FHIR-bundle.json'), 'utf8')
-
-  return JSON.parse(data);
+  return JSON.parse(data) as fhir4.Bundle;
 }
 
+export const evaluateCMS122ForPatient = async (patientBundle: fhir4.Bundle[], options: CalculationOptions = {}) => {
+  const measureBundle = await loadCMS122();
+  return await Calculator.calculateMeasureReports(measureBundle, patientBundle, options)
+}
