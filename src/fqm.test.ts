5f93e47fc4095a357b60ada51947f501cad5b06a
radamson <radamson@users.noreply.github.com>
2022-11-22 15:16:03 -0500

    lint and style

--- src/fqm.test.ts
+++ src/fqm.test.ts
@@ -3,31 +3,31 @@ import { readFile } from 'fs/promises';
 import * as path from 'path';
 
 const loadPatientBundle = async (filename: string): Promise<fhir4.Bundle[]> => {
-  const data = await readFile(path.join(__dirname, '__fixtures__', filename), 'utf8')
+  const data = await readFile(path.join(__dirname, '__fixtures__', filename), 'utf8');
   return [JSON.parse(data)] as fhir4.Bundle[];
-}
+};
 
 test('loadCMS122 works', async () => {
-  const cms122 = await loadCMS122()
+  const cms122 = await loadCMS122();
   expect(cms122.resourceType).toBe('Bundle');
-})
+});
 
 test('evaluateCMS122ForPatient against denom', async () => {
   const denom = await loadPatientBundle('tests-denom-CMS122-Patient-bundle.json');
-  const results = await evaluateCMS122ForPatient(denom,
-                                                 { 'measurementPeriodStart': '2019-01-01T00:00:00-07:00',
-                                                    'measurementPeriodEnd': '2019-12-31T00:00:00-07:00'}
-                                                );
+  const results = await evaluateCMS122ForPatient(denom, {
+    measurementPeriodStart: '2019-01-01T00:00:00-07:00',
+    measurementPeriodEnd: '2019-12-31T00:00:00-07:00',
+  });
   let mr;
   if (Array.isArray(results.results)) {
     expect(results.results.length).toBe(1);
-    mr = results.results[0]
+    mr = results.results[0];
   } else {
-    mr = results.results
+    mr = results.results;
   }
-  expect(mr.group).not.toBeUndefined()
+  expect(mr.group).not.toBeUndefined();
   if (mr.group != undefined) {
     expect(mr.group.length).toBe(1);
     expect(mr.group[0]?.measureScore?.value).toBe(1);
   }
-})
+});
--- src/fqm.ts
+++ src/fqm.ts
@@ -4,15 +4,15 @@ import * as path from 'path';
 import { CalculationOptions } from 'fqm-execution/build/types/Calculator';
 
 export const loadMeasureFromFile = async (filename: string): Promise<fhir4.Bundle> => {
-  const data = await readFile(path.join(__dirname, 'measures', filename), 'utf8')
+  const data = await readFile(path.join(__dirname, 'measures', filename), 'utf8');
   return JSON.parse(data) as fhir4.Bundle;
-}
+};
 
 export const loadCMS122 = async (): Promise<fhir4.Bundle> => {
-  return loadMeasureFromFile('DiabetesHemoglobinA1cHbA1cPoorControl9FHIR-bundle.json')
-}
+  return loadMeasureFromFile('DiabetesHemoglobinA1cHbA1cPoorControl9FHIR-bundle.json');
+};
 
 export const evaluateCMS122ForPatient = async (patientBundle: fhir4.Bundle[], options: CalculationOptions = {}) => {
   const measureBundle = await loadCMS122();
-  return await Calculator.calculateMeasureReports(measureBundle, patientBundle, options)
-}
+  return await Calculator.calculateMeasureReports(measureBundle, patientBundle, options);
+};
