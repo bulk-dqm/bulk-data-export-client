4c874f52a18597ee7a64e1d69a09ae699ad26171
radamson <radamson@users.noreply.github.com>
2022-11-22 15:03:15 -0500

    generally load measures from a file

--- src/fqm.ts
+++ src/fqm.ts
@@ -3,9 +3,13 @@ import { Calculator } from 'fqm-execution';
 import * as path from 'path';
 import { CalculationOptions } from 'fqm-execution/build/types/Calculator';
 
-export const loadCMS122 = async (): Promise<fhir4.Bundle> => {
-  const data = await readFile(path.join(__dirname, 'measures', 'DiabetesHemoglobinA1cHbA1cPoorControl9FHIR-bundle.json'), 'utf8')
+export const loadMeasureFromFile = async (filename: string): Promise<fhir4.Bundle> => {
+  const data = await readFile(path.join(__dirname, 'measures', filename), 'utf8')
   return JSON.parse(data) as fhir4.Bundle;
+}
+
+export const loadCMS122 = async (): Promise<fhir4.Bundle> => {
+  return loadMeasureFromFile('DiabetesHemoglobinA1cHbA1cPoorControl9FHIR-bundle.json')
 }
 
 export const evaluateCMS122ForPatient = async (patientBundle: fhir4.Bundle[], options: CalculationOptions = {}) => {
