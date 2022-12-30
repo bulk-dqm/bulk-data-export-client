30bedd6b47f9d93e026d19ae53b416c720f49ff4
Sarah McDougall <87094479+sarahmcdougall@users.noreply.github.com>
2022-12-30 07:53:35 -0500

    Add support for file download path (#3)
    * find abs path from rel path
    
    * add handling for non-existent directory
    
    * prettier fix
    
    * change prompt dep to built-in rl
    
    * prettier fix

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { resolve } from 'path';
+import * as readline from 'readline/promises';
+import fs from 'fs';
+import 'colors';
 import { BulkDataClient as Types } from 'bulk-data-client';
 import BulkDataClient from 'bulk-data-client/built/lib/BulkDataClient';
 import CLIReporter from 'bulk-data-client/built/reporters/cli';
@@ -10,12 +14,18 @@ const program = new Command();
 program
   .requiredOption('-f, --fhir-url <url>', 'Base URL of FHIR server used for data retrieval')
   .requiredOption('-g, --group <id>', 'FHIR Group ID used to query FHIR server for resources')
-  .option('-d, --destination <destination>', 'Download destination. Defaults to ./downloads', './downloads')
+  .option(
+    '-d, --destination <destination>',
+    'Download destination relative to current working directory. Defaults to ./downloads',
+    `${process.cwd()}/downloads`
+  )
   .option('-p, --parallel-downloads <number>', 'Number of downloads to run in parallel. Defaults to 1.', '1')
   .parseAsync(process.argv);
 
 // add required trailing slash to FHIR URL if not present
 program.opts().fhirUrl = program.opts().fhirUrl.replace(/\/*$/, '/');
+// get absolute path for specified destination directory
+program.opts().destination = resolve(program.opts().destination);
 
 const main = async () => {
   const requests = {
@@ -33,6 +43,23 @@ const main = async () => {
     ...program.opts(),
     requests,
   } as Types.NormalizedOptions;
+
+  if (!fs.existsSync(program.opts().destination)) {
+    console.log(`Destination ${program.opts().destination} does not exist.`);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    const answer = await rl.question('Make new directory? [y/n]');
+    if (answer.toLowerCase() === 'y') {
+      fs.mkdirSync(program.opts().destination, { recursive: true });
+    } else {
+      console.error('Exiting due to non-existent destination.'.red);
+      process.exit();
+    }
+    rl.close();
+  }
+
   const client = new BulkDataClient(options as Types.NormalizedOptions);
   CLIReporter(client);
   const statusEndpoint = await client.kickOff();
