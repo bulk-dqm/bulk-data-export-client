8ef3b66dc097eef3a7e31cb2c6d98f9af90cee6f
sarahmcdougall <smcdougall@mitre.org>
2023-01-09 10:47:01 -0500

    import logging and add logger CLI option

--- src/cli.ts
+++ src/cli.ts
@@ -8,6 +8,8 @@ import 'colors';
 import { BulkDataClient as Types } from 'bulk-data-client';
 import BulkDataClient from 'bulk-data-client/built/lib/BulkDataClient';
 import CLIReporter from 'bulk-data-client/built/reporters/cli';
+import * as Logger from 'bulk-data-client/built/loggers/index';
+
 const program = new Command();
 
 // specify options for bulk data request and retrieval
@@ -20,12 +22,16 @@ program
     `${process.cwd()}/downloads`
   )
   .option('-p, --parallel-downloads <number>', 'Number of downloads to run in parallel. Defaults to 1.', '1')
+  .option(
+    '-l, --log-items [file-path]',
+    'Path to a log file (if logging is desired). Defaults to log.ndjson and is stored in the download destination directory (if logging enabled), false otherwise'
+  )
   .parseAsync(process.argv);
 
 // add required trailing slash to FHIR URL if not present
 program.opts().fhirUrl = program.opts().fhirUrl.replace(/\/*$/, '/');
 // get absolute path for specified destination directory
-program.opts().destination = resolve(program.opts().destination);
+const destination = resolve(program.opts().destination);
 
 const main = async () => {
   const requests = {
@@ -41,18 +47,19 @@ const main = async () => {
 
   const options = {
     ...program.opts(),
+    destination,
     requests,
   } as Types.NormalizedOptions;
 
-  if (!fs.existsSync(program.opts().destination)) {
-    console.log(`Destination ${program.opts().destination} does not exist.`);
+  if (!fs.existsSync(destination)) {
+    console.log(`Destination ${destination} does not exist.`);
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
     const answer = await rl.question('Make new directory? [y/n]');
     if (answer.toLowerCase() === 'y') {
-      fs.mkdirSync(program.opts().destination, { recursive: true });
+      fs.mkdirSync(destination, { recursive: true });
     } else {
       console.error('Exiting due to non-existent destination.'.red);
       process.exit();
@@ -62,6 +69,55 @@ const main = async () => {
 
   const client = new BulkDataClient(options as Types.NormalizedOptions);
   CLIReporter(client);
+
+  if (program.opts().logItems) {
+    let logFile;
+    program.opts().logItems === true
+      ? (logFile = `${destination}/log.ndjson`)
+      : (logFile = `${destination}/${program.opts().logItems}`);
+    const logger = Logger.createLogger({ enabled: true, file: logFile } as Types.LoggingOptions);
+    const startTime = Date.now();
+
+    client.on('kickOffEnd', ({ requestParameters, capabilityStatement, response }: any) => {
+      logger.log('info', {
+        eventId: 'kickoff',
+        eventDetail: {
+          exportUrl: response.requestUrl,
+          errorCode: response.statusCode >= 400 ? response.statusCode : null,
+          errorBody: response.statusCode >= 400 ? response.body : null,
+          softwareName: capabilityStatement.software?.name || null,
+          softwareVersion: capabilityStatement.software?.version || null,
+          softwareReleaseDate: capabilityStatement.software?.releaseDate || null,
+          fhirVersion: capabilityStatement.fhirVersion || null,
+          requestParameters,
+        },
+      });
+    });
+
+    client.on('allDownloadsComplete', (downloads: any[]) => {
+      const eventDetail = {
+        files: 0,
+        resources: 0,
+        bytes: 0,
+        attachments: 0,
+        duration: Date.now() - startTime,
+      };
+
+      downloads.forEach((d) => {
+        eventDetail.files += 1;
+        eventDetail.resources += d.resources;
+        eventDetail.bytes += d.uncompressedBytes;
+        eventDetail.attachments += d.attachments;
+      });
+
+      logger.log('info', { eventId: 'export_complete', eventDetail });
+    });
+
+    client.on('downloadComplete', (eventDetail: any) => {
+      logger.log('info', { eventId: 'download_complete', eventDetail });
+    });
+  }
+
   const statusEndpoint = await client.kickOff();
   const manifest = await client.waitForExport(statusEndpoint);
   await client.downloadAllFiles(manifest);
