46621cf8677df14471cd22bc55925902bb979a10
radamson <radamson@users.noreply.github.com>
2023-01-11 08:47:12 -0500

    remove code for resolving key from url

--- src/jwk.ts
+++ src/jwk.ts
@@ -3,34 +3,10 @@ import { readFile } from 'fs/promises';
 import * as path from 'path';
 import * as http from 'http';
 
-export const resolveJWK = (keyLocation: string): Promise<jose.JWK.Key> => {
-  if (keyLocation.startsWith('http://') || keyLocation.startsWith('https://')) {
-    return loadJWKFromURL(keyLocation);
-  } else {
-    return loadJWKFromFile(keyLocation);
-  }
+export const resolveJWK = async (keyLocation: string): Promise<jose.JWK.Key> => {
+  const jwk = await readFile(path.join(process.cwd(), keyLocation), 'utf-8');
+  const jwk_key = jose.JWK.asKey(JSON.parse(jwk));
+  jwk_key.toPEM();
+  return jwk_key;
 };
 
-const loadJWKFromFile = async (filename: string): Promise<jose.JWK.Key> => {
-  const jwk = await readFile(path.join(process.cwd(), filename), 'utf-8');
-  const jwky = jose.JWK.asKey(JSON.parse(jwk));
-  jwky.toPEM();
-  return jwky;
-};
-
-
-const loadJWKFromURL = async (url: string): Promise<jose.JWK.Key> => {
-  const promise = new Promise<jose.JWK.Key>((resolve) => {
-    http.get(url, (res) => {
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => {
-        rawData += chunk;
-      });
-      res.on('end', () => {
-        resolve(jose.JWK.asKey(JSON.parse(rawData)));
-      });
-    });
-  });
-  return promise;
-};
