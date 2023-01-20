71bfa5dfae3284eb5984287a1dbaed8a9bfa179c
sarahmcdougall <smcdougall@mitre.org>
2023-01-20 11:49:07 -0500

    add num polling requests to report

--- src/reportGenerator.ts
+++ src/reportGenerator.ts
@@ -20,12 +20,16 @@ export const createExportReport = async (downloadDir: string, file: string) => {
   const kickoffResults = exportEvents.filter((log) => log.eventId === 'kickoff')[0];
   const exportCompleteResults = exportEvents.filter((log) => log.eventId === 'export_complete')[0];
   const downloadResults = exportEvents.filter((log) => log.eventId === 'download_complete');
+  const numPollingRequests = exportEvents.filter((log) =>
+    ['status_progress', 'status_complete', 'status_error'].includes(log.eventId)
+  ).length;
 
   const htmlContent = `
     <div>
       <h1>Export Results for Export ${exportId}</h1>
       <p>Export Timestamp: ${kickoffResults.timestamp}</p>
       <p>Export URL: ${kickoffResults.eventDetail.exportUrl}</p>
+      <p>Number of Polling Requests: ${numPollingRequests}</p>
       <h2>Completed Export Information</h2>
       <p>Downloads Complete Timestamp: ${exportCompleteResults.timestamp}</p>
       <p>Event Details:
